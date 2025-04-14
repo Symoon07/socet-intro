bfbbd8703c2eee539ed0f98d93991fe0341d1143
Devin Singh <drsingh2518@icloud.com>
2025-04-14 00:16:26 -0400

    systems: lab2

---
+++ systems/lab2/src/accelerator.c
@@ -0,0 +1,12 @@
+#include "accelerator.h"
+
+// TODO: Fill in BASE here
+#define BASE 0x0
+
+struct Accelerator {
+    // TODO: match your memory map here
+};
+
+Accelerator *initAccelerator() { return (Accelerator *)BASE; }
+
+// TODO: Implement your functions here
---
+++ systems/lab2/src/accelerator.h
@@ -0,0 +1,10 @@
+#ifndef ACCELERATOR_H
+#define ACCELERATOR_H
+
+typedef struct Accelerator Accelerator;
+
+Accelerator *initAccelerator();
+
+// TODO: Write all of your functions here!
+
+#endif
